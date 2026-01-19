a639acff833f21d161baa8198ed97482af0e4379
shadowen <168087747+owltastic676@users.noreply.github.com>
2026-01-19 14:59:25 +0100

    Retirer valeur par defaut
    Plus de valeur par défaut dans l'affichage des héros

--- src/pages/heroes/heroes.vue
+++ src/pages/heroes/heroes.vue
@@ -8,16 +8,6 @@ const focusedIndex = ref(0)
 
 onMounted(async () => {
   await store.fetchHeroes()
-
-  if (store.heroes.length === 0) {
-    store.heroes = [
-      { HeroId: 1, Name: 'Warrior', MaxHP: 100, Dodge: 5, Protection: 10, Speed: 10, AccuracyModifier: 0, CriticChance: 2, Damage: 15, ResListId: 1, MiscellaneousId: 1 },
-      { HeroId: 2, Name: 'Ranger', MaxHP: 80, Dodge: 15, Protection: 5, Speed: 14, AccuracyModifier: 5, CriticChance: 10, Damage: 12, ResListId: 2, MiscellaneousId: 2 },
-      { HeroId: 1, Name: 'Warrior', MaxHP: 100, Dodge: 5, Protection: 10, Speed: 10, AccuracyModifier: 0, CriticChance: 2, Damage: 15, ResListId: 1, MiscellaneousId: 1 },
-      { HeroId: 2, Name: 'Ranger', MaxHP: 80, Dodge: 15, Protection: 5, Speed: 14, AccuracyModifier: 5, CriticChance: 10, Damage: 12, ResListId: 2, MiscellaneousId: 2 },
-      { HeroId: 3, Name: 'Mage', MaxHP: 60, Dodge: 5, Protection: 0, Speed: 9, AccuracyModifier: 10, CriticChance: 20, Damage: 25, ResListId: 3, MiscellaneousId: 3 }
-    ]
-  }
 })
 
 const prevCard = () => {

6768c1a68a9ed42e32ef0b281d5392ce4580593f
Aurélienxx <aurelienfontaine09@gmail.com>
2026-01-19 13:47:29 +0100

    ajout hashage mdp + ajout .env

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import userRoutes from './routes/users.routes.js'
-import authRoutes from './routes/auth.routes.js'
-import heroesRoutes from './routes/heroes.routes.js'
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/users', userRoutes)
-app.use('/authentification', authRoutes)
-app.use('/heroes', heroesRoutes)
+import "./config/db.js";
+
+import userRoutes from "./routes/users.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import heroesRoutes from "./routes/heroes.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/authentification", authRoutes);
+app.use("/heroes", heroesRoutes);
 
 app.listen(3000, () => {
-  console.log('Serveur lancé sur http://localhost:3000')
-})
+  console.log("Serveur lancé sur http://localhost:3000");
+});
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./backend/.env" });
+
 import db from '../config/db.js'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import { createUserInDb } from '../services/users.service.js'
 
-const JWT_SECRET = "tonSecretSuperSecret" // tu peux mettre ça dans .env
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const loginUser = (req, res) => {
   const { Username, Password } = req.body
@@ -10,49 +14,45 @@ export const loginUser = (req, res) => {
   db.query(
     "SELECT * FROM users WHERE Username = ?", [Username], 
     (err, result) => {
-      if (err) 
-        return res.status(500).json(err)
+      if (err) return res.status(500).json(err)
 
       if (result.length === 0) 
         return res.status(404).json({ message: "Utilisateur non trouvé" })
 
       const user = result[0]
 
-      // Vérification du mot de passe
-          if ((Password === user.Password) === false) {
-            return res.status(401).json({ message: "Mot de passe incorrect" })
-          }
+      const isValid = bcrypt.compareSync(Password, user.Password)
+      if (!isValid) {
+        return res.status(401).json({ message: "Mot de passe incorrect" })
+      }
 
-      // Création d’un token JWT
       const token = jwt.sign({ UserId: user.UserId, Username: user.Username }, JWT_SECRET, { expiresIn: '1h' })
 
       res.json({ token })
   })
 }
 
-// Si un utilisateur avec le même pseudonyme existe déjà, la création d'utilisateur est annulée.
 export const registerUser = (req, res) => {
-  console.log("BODY REÇU :", req.body)
   const { LastName, FirstName, Password, Username } = req.body;
 
   db.query(
     "SELECT * FROM users WHERE Username = ?", [Username], 
     (err, result) => {
-      if (err)
-        return res.status(500).json(err);
+      if (err) return res.status(500).json(err);
 
       if (result.length !== 0) {
         return res.status(400).json({ message: "Utilisateur avec le même pseudonyme existe déjà" })
       }
 
+      const saltRounds = 10;
+      const hashedPassword = bcrypt.hashSync(Password, saltRounds);
+
       createUserInDb(
-        { LastName, FirstName, Password, Username }, 
+        { LastName, FirstName, Password: hashedPassword, Username }, 
         (err, result) => {
-          if (err) 
-            return res.status(500).json(err)
+          if (err) return res.status(500).json(err)
           
           const token = jwt.sign({UserId: result.insertId, Username}, JWT_SECRET, {expiresIn: '1h'})
-
           return res.json({ token })
         }
       )
@@ -64,12 +64,10 @@ export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (!token)
-    return res.status(401).json({ message: "Token manquant" })
+  if (!token) return res.status(401).json({ message: "Token manquant" })
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err)
-      return res.status(403).json({ message: "Token invalide" })
+    if (err) return res.status(403).json({ message: "Token invalide" })
 
     req.user = user
     next()
